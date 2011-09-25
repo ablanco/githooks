5ac376145bb1b11e979f3f08eb2f133ddac33a3f
Alejandro Blanco <alejandro.b.e@gmail.com>
2011-09-25 12:44:57 +0200

    Transform from git date to python datetime

--- githooks/git.py
+++ githooks/git.py
@@ -15,18 +15,44 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
 import subprocess
-
 
 GLOBAL_CONTEXT = 'global'
 REPOSITORY_CONTEXT = 'local'
 SYSTEM_CONTEXT = 'system'
+
+__MONTHS = {
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3,
+    'Apr': 4,
+    'May': 5,
+    'Jun': 6,
+    'Jul': 7,
+    'Ago': 8,
+    'Sep': 9,
+    'Oct': 10,
+    'Nov': 11,
+    'Dic': 12,
+}
 
 
 def __execute_command(proc):
     p = subprocess.Popen(proc, stdout=subprocess.PIPE)
     out, err = p.communicate()
     return out
+
+
+def __gittime2datetime(git_date):
+    git_date = git_date.split(' ')
+    day = int(git_date[2])
+    month = __MONTHS[git_date[1]]
+    year = int(git_date[4])
+    time = git_date[3].split(':')
+    #time_zone = TODO
+    return datetime.datetime(year, month, day, int(time[0]), int(time[1]),
+                             int(time[2]))
 
 
 def config(family, variable, context):
@@ -48,7 +74,8 @@ def author(revision):
 
 def date(revision):
     log = __execute_command(['git', 'log', revision, '-n', '1']).splitlines()
-    return log[3][8:]
+    date = log[3][8:]
+    return __gittime2datetime(date)
 
 
 def file_names(old_revision, revision):
